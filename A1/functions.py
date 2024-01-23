426f2c864f38ed5ba84af3e6e6f282c2a961072e
Brandon Parker <brandonparker119@gmail.com>
2024-01-23 16:21:37 -0500

    Staring code

---
+++ A1/functions.py
@@ -0,0 +1,7 @@
+def create_inverted_index_from_files(folder_path):
+
+
+    inverted_index = []
+
+
+    return inverted_index
---
+++ A1/main.py
@@ -0,0 +1,3 @@
+from functions import create_inverted_index_from_files
+
+invereted_index = create_inverted_index_from_files("./data")
